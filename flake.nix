18f74d572c9cebab71e7d4c8386ce7b7ce7b3ca6
Jake Hamilton <jake.hamilton@hey.com>
2023-11-26 02:51:13 -0800

    chore: upgrade nixpkgs to avoid bun bug

--- flake.nix
+++ flake.nix
@@ -2,8 +2,7 @@
   description = "Snowfall Thaw";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
-    unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     snowfall-lib = {
       url = "github:snowfallorg/lib?ref=v2.1.1";
